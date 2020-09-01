92ad9cf64704148fb1576ebb48fb1d4633ed276e
Julian Stephens <julianstephens55@gmail.com>
2020-09-01 18:28:30 -0400

    Add scss functionality

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 from flask import Flask, render_template
-#  from flask.ext.scss import Scss
+from flask_assets import Environment, Bundle
 import contentful
 
 
 app = Flask(__name__)
+
+# Configure flask assets for sass
+assets = Environment(app)
+assets.url = app.static_url_path
+scss = Bundle('_variables.scss', 'main.scss',
+              filters='pyscss', output='main.css')
+assets.register('scss_all', scss)
+
+
+# Load custom config
 app.config.from_object("config.Config")
+
+# Connect to contentful client
 client = contentful.Client(
     app.config["CONTENTFUL_SPACEID"], app.config["CONTENTFUL_ACCESSKEY"])
 
 
 def getSiteName():
+    """Retrieves site name from contentful
+
+    Returns:
+        string: Site name
+    """
     entry_id = '1EngZagyUcVRislD4wINfw'
     name = client.entry(entry_id)
     return name.text
 
 
 def getEvents():
+    """Retrieves all events from contentful
+
+    Returns:
+        list: Events in order from oldest to newest
+    """
     events = client.entries(
         {'content_type': 'event', 'order': 'fields.date'})
 
@@ -28,6 +50,11 @@ def getEvents():
 
 
 def getNav():
+    """Retrieves nav items from contentful
+
+    Returns:
+        list: Nav items in display order
+    """
     navItems = client.entries(
         {'content_type': 'navBar', 'order': 'fields.order'})
 
